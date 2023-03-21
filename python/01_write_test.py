92754e41b60e5560fe89133a47d5cb70193033b8
AdnenMess <adnen_messaoudi@hotmail.com>
2023-03-21 18:31:53 +0100

    add a timeout

--- python/01_write_test.py
+++ python/01_write_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+from pathlib import Path
 
 # Import the new influxdb API client (pip install influxdb_client)
 import influxdb_client
@@ -8,7 +9,9 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 # Read the csv into a dataframe
 #df = pd.read_csv("data/test.csv", )
 
-df = pd.read_csv("data/PRSA_Data_Aotizhongxin_20130301-20170228.csv", )
+folder = Path.cwd().parent / "Data" / "PRSA"
+
+df = pd.read_csv(folder/"PRSA_Data_Aotizhongxin_20130301-20170228.csv", )
 
 
 # create a timestamp out of the four columns
@@ -24,21 +27,22 @@ ex_df = df.drop(['year', 'month', 'day', 'hour','No'], axis=1)
 print(ex_df)
 
 
-Fields = ['PM2.5','PM10','SO2','NO2','CO','O3','TEMP','PRES','DEWP','RAIN','wd','WSPM']
+Fields = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3', 'TEMP', 'PRES', 'DEWP', 'RAIN', 'wd', 'WSPM']
 # Define tag fields
-datatags = ['station','wd']
+datatags = ['station', 'wd']
 
 
 client = influxdb_client.InfluxDBClient(
    url='http://localhost:8086',
-   token='lTUKuRE46dJw8Yj_AmYtQHELsnfNM1eGVdJkYUj_Q_Ddq7yqCScDlbt9PYdu-RR_OW-NX9S_GaxNqXz7iAECCw==',
+   token='4dBQ5ZpeBq65N7EPfegmLOxwAwmWc6P_E5R1joJ_d8wFW2tpxMr2DzBEbLKsf_s4wqe1mG4uyobDmSw2cSe_5g==',
    org='my-org'
 )
 
 #write the test data into measurement
-write_api = client.write_api(write_options=SYNCHRONOUS)
+write_api = client.write_api(write_options=SYNCHRONOUS, timeout=50000)
 #message = write_api.write(bucket='air_quality',org='my-org',record = ex_df, data_frame_measurement_name = 'test', data_frame_tag_columns=['station','wd'])
-message = write_api.write(bucket='air-quality',org='my-org',record = ex_df, data_frame_measurement_name = 'test', data_frame_tag_columns=['station','wd'])
+message = write_api.write(bucket='air-quality', org='my-org', record=ex_df,
+                          data_frame_measurement_name='test', data_frame_tag_columns=['station', 'wd'])
 print(message)
 
 
